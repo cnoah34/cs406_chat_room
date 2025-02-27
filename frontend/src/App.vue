e74c818d2ce4d0a18d7f65cbc617791b904fb421
Colby Noah <colbynoah@gmail.com>
2025-02-27 01:49:54 -0800

    Began work on frontend, have a working sign up component so far

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Home from './components/Home.vue'
+import SignUp from './components/SignUp.vue'
+import Login from './components/Login.vue'
+</script>
+
+<template>
+    <SignUp />
+    <!--
+    <div class="parent-container">
+        <SignUp />
+    </div>
+    -->
+</template>
+
+<style>
+.parent-container {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="container">
+        <h1>Login</h1>
+        <form @submit.prevent="handleSubmit" class="fields">
+            <input type="text" id="username" placeholder="Username" v-model="formData.username">
+            <input type="password" id="password" placeholder="Password" v-model="formData.password">
+            <button type="submit">Sign up</button>
+        </form>
+        <p style="padding-left: 20px">Forgot password? Good luck!</p>
+        <p>Don't have an account? <a style="color: var(--vue-green)">Sign up</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                formData: {
+                    username: '',
+                    password: ''
+                }
+            };
+        },
+        methods: {
+            handleSubmit() {
+                console.log('Form data: ', this.formData);
+            }
+        }
+    };
+</script>
+
+<style>
+.container {
+    width: 400px;
+    height: 300px;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: var(--foreground);
+    border: 4px solid var(--vue-green);
+}
+
+.fields {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.fields input {
+    width: 300px;
+    height: 40px;
+    font-size: 12pt;
+    margin-bottom: 10px;
+}
+
+.fields button {
+    width: 70px;
+    height: 30px;
+    font-size: 11pt;
+    margin-top: 10px;
+}
+
+h1 {
+    color: var(--vue-green);
+    margin-top: 20px;
+}
+
+a {
+}
+
+</style>
